6272a8f7f4585b534076f81e74cf51e76438d956
Forrest Blade <forrestblade.code@gmail.com>
2019-07-15 12:12:01 -0500

    feat: update projects

--- src/application/projects/index.js
+++ src/application/projects/index.js
@@ -3,7 +3,7 @@ import doggo from '../../images/doggo.png'
 import dealerlocator from '../../images/dealerlocator.png'
 import vendy from '../../images/vendy.png'
 import pomodoro from '../../images/pomodoro.png'
-import rgb from '../../images/rgb.png'
+import csv from '../../images/csv-parse.png'
 import lunchbox from '../../images/lunchbox.png'
 
 const Projects = () => {
@@ -11,8 +11,8 @@ const Projects = () => {
     <div>
       <section className='cf mt5 pv5 bt b--black-05 ph6-l'>
         <h1 className='tc f5 ttu fw6 tracked mb4 dark-green avenir'>Projects I have worked on</h1>
-        <a href='https://forrestblade.github.io' className='fl w-third w-25-ns border-box overflow-hidden ba bw2 white' title>
-          <div className='grow cover bg-center pv5 pv6-l' style={{ backgroundImage: `url(${rgb})` }} />
+        <a href='https://csv-json-parse.netlify.com' className='fl w-third w-25-ns border-box overflow-hidden ba bw2 white' title>
+          <div className='grow cover bg-center pv5 pv6-l' style={{ backgroundImage: `url(${csv})` }} />
         </a>
         <a href='https://github.com/forrestblade/lunchbox' className='fl w-third w-25-ns border-box overflow-hidden  ba bw2 white' title>
           <div className='grow cover bg-top pv5 pv6-l' style={{ backgroundImage: `url(${lunchbox})` }} />
