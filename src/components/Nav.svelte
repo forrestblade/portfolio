82f8b91ead20040bc33c7947a78d77c60e29bbdb
Forrest Blade <forrestblade.code@gmail.com>
2020-02-21 01:26:27 -0600

    feat: update content and layout

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,23 +1,38 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
-<nav class="dt w-100 border-box pa3 ph5-ns code">
-  <a class="dtc v-mid dark-green hover-purple link dim w-25 {segment === undefined ? "selected" : ""}"  href='.' title="home">
+<nav class="dt w-100 border-box pa3 mw9 center code">
+  <a
+    class="dtc v-mid dark-green hover-purple link dim w-25 {segment === undefined ? 'selected' : ''}"
+    href="."
+    title="home">
     Forrest Blade
-	<br/>
-	<small class="dark-gray">brutalist web application developer</small>
+    <br />
+    <small class="dark-gray">brutalist web application developer</small>
   </a>
   <div class="dtc v-mid w-75 tr">
-    <a class="link dim hover-light-green dark-gray f6 f5-ns dib mr3 mr4-ns {segment === "blog" ? "selected" : ""}" rel=prefetch href='blog' title="blog">λ blog</a>
+    <a
+      class="link dim hover-light-green dark-gray f6 f5-ns ph2 dib {segment === 'blog' ? 'selected' : ''}"
+      rel="prefetch"
+      href="blog"
+      title="blog">
+      λ blog
+    </a>
   </div>
 </nav>
-  <section class="pa3 tr w-100 ph5-ns">
-    <a class="f3 link hover-dark-blue b no-underline black dib ph2 pv1" href="./#portfolio">portfolio</a>
-    <a class="f3 link b hover-light-green no-underline black dib ph2 pv1" href="/about">about</a>
-    <a class="f3 link b hover-orange no-underline black dib ph2 pv1" href="/gifs">gifs</a>
-    <a class="f3 link b hover-red no-underline black dib ph2 pv1" href="https://open.spotify.com/playlist/37i9dQZEVXcKkDdmEwVelx?si=zIgC29xTRHymIJdFFV0x9w">music</a>
-    <a class="f3 link b hover-light-green no-underline black dib ph2 pv1" href="https://github.com/forrestblade">github</a>
-    <a class="f3 link b hover-pink no-underline black dib ph2 pv1" href="https://instagram.com/forrest.blade">instagram</a>
-    <a class="f3 link b hover-purple no-underline black dib ph2 pv1" href="https://twitter.com/codecoffeedoggo">twitter</a>
-  </section>
+<section class="pa3 tr mw9 center w-100">
+  <a
+    class="f3 link hover-dark-blue b no-underline black dib ph2 pv1"
+    href="./#portfolio">
+    portfolio
+  </a>
+  <a
+    class="f3 link b hover-light-green no-underline black dib ph2 pv1"
+    href="/about">
+    about
+  </a>
+  <a class="f3 link b hover-orange no-underline black dib ph2 pv1" href="/gifs">
+    gifs
+  </a>
+</section>
--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -8,13 +8,13 @@
   );
 </script>
 
-<article class="pa3 pa5-ns" data-name="slab-stat-small">
+<article class="pa3 ph3 pa5-ns" data-name="slab-stat-small">
   <div class="cf">
     <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
       <dd class="f6 fw4 ml0">Email</dd>
       <dd class="f3 fw6 ml0">
         <a
-          class="pointer hover-red link dim"
+          class="pointer hover-light-red link dim"
           href="mailto:forrestblade.code@gmail.com">
           lets collab
         </a>
@@ -24,35 +24,35 @@
       <dd class="f6 fw4 ml0">Resume</dd>
       <dd class="f3 fw6 ml0">
         <a
-          class="link hover-green mr5-l"
+          class="link hover-green"
           href="https://drive.google.com/file/d/1QX1-TV8vL74Ry15bDhFxKzSVZvbKuYso/view">
           here
         </a>
       </dd>
     </dl>
     <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd class="f6 fw4 ml0">Github</dd>
+      <dd class="f3 fw6 ml0">
+        <a
+          class="link hover-blue"
+          href="https://github.com/forrestblade">
+          overview
+        </a>
+      </dd>
+    </dl>
+    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
       <dd class="f6 fw4 ml0">Days Old</dd>
-      <dd class="f3 fw6 ml0">{diffDays}</dd>
+      <dd class="f3 fw6 ml0 hover-yellow">{diffDays}</dd>
     </dl>
     <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
       <dd class="f6 fw4 ml0">Favorite Dog</dd>
-      <dd class="f3 fw6 ml0">All of Them</dd>
-    </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Song</dd>
-      <dd class="f3 fw6 ml0">
-        <a
-          class="link hover-green "
-          href="https://open.spotify.com/track/51rPRW8NjxZoWPPjnRGzHw?si=rR8ZYhUOSIiKqFEMWVsA8g">
-          tadow
-        </a>
-      </dd>
+      <dd class="f3 fw6 ml0 hover-red">All of Them</dd>
     </dl>
     <dl class="fl fn-l w-50 dib-l w-auto-l lh-title">
       <dd class="f6 fw4 ml0">Movie</dd>
       <dd class="f3 fw6 ml0">
         <a
-          class="link hover-green"
+          class="link hover-light-green"
           href="https://en.wikipedia.org/wiki/Pom_Poko">
           pom poko
         </a>
@@ -65,8 +65,8 @@
   <p class="measure lh-copy">
     I work with a lot of modern web technologies, and I am always looking for
     new skills to learn. I am interested in how the browser can be a delivery
-    module for all sorts of content. I'm currently working through some machine
-    learning courses, so check back soon for updates on that. Shoot me an
+    module for all sorts of content. I'm focused on applying simple, minimal
+    machine learning solutions. Shoot me an
     <a
       class="pointer hover-red underline dim dark-green"
       href="mailto:forrestblade.code@gmail.com">
@@ -75,16 +75,10 @@
     and we can start a conversation. If you would like to view my resume it is
     available
     <a
-      href="https://drive.google.com/file/d/1QX1-TV8vL74Ry15bDhFxKzSVZvbKuYso/view"
+      href="https://docs.google.com/document/d/1Rr7gn03E5Rnp3wUfPElH0LHtBvTNsXTzKs9HKg-2l6g/edit?usp=sharing"
       class="pointer hover-red underline dim dark-green">
       here
     </a>
     .
   </p>
-  <!-- <p class="measure lh-copy">
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-    vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet.
-  </p> -->
 </article>
